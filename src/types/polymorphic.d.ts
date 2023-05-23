9e9512678f6bafbc63125fd8ff86b94571cdcf3e
gomgun <zxolqs@gmail.com>
2023-05-23 16:40:11 +0900

    [#10] feat : Add types of polymorphic component

---
+++ src/types/polymorphic.d.ts
@@ -0,0 +1,16 @@
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = object,
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>["ref"];
+
+export type { PolymorphicComponentProp, PolymorphicRef };
