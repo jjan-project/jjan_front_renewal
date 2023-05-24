928d34d2c51d0954aa7a21561434b59b9943226f
MinGyu-Kang <kagrin97@gmail.com>
2023-05-24 13:33:43 +0900

    [#15] test : Add test for signupSchema
    schema/register 파일의 signupSchema 테스트 코드를 작성했습니다.

---
+++ src/schema/__test__/register.test.ts
@@ -0,0 +1,69 @@
+import { describe, it, expect } from "vitest";
+
+import { signupSchema } from "../register";
+
+const testData = {
+  validEmail: "test@example.com",
+  validPassword: "Test1234!",
+  invalidEmail: "invalidemail",
+  invalidPassword: "Test12345678",
+  mismatchedPassword: "Test1234!@",
+  emptyString: "",
+};
+
+describe("회원가입 스키마", () => {
+  it("유효한 데이터로 통과해야 함", () => {
+    const validData = {
+      email: testData.validEmail,
+      password: testData.validPassword,
+      confirmPassword: testData.validPassword,
+    };
+
+    const validationResult = signupSchema.safeParse(validData);
+    expect(validationResult.success).toBe(true);
+  });
+
+  it("필수 필드 누락 시 실패해야 함", () => {
+    const invalidData = {
+      email: testData.emptyString,
+      password: testData.emptyString,
+      confirmPassword: testData.emptyString,
+    };
+
+    const validationResult = signupSchema.safeParse(invalidData);
+    expect(validationResult.success).toBe(false);
+  });
+
+  it("유효하지 않은 이메일일 때 실패해야 함", () => {
+    const invalidData = {
+      email: testData.invalidEmail,
+      password: testData.validPassword,
+      confirmPassword: testData.validPassword,
+    };
+
+    const validationResult = signupSchema.safeParse(invalidData);
+    expect(validationResult.success).toBe(false);
+  });
+
+  it("유효하지 않은 비밀번호일 때 실패해야 함", () => {
+    const invalidData = {
+      email: testData.validEmail,
+      password: testData.invalidPassword,
+      confirmPassword: testData.invalidPassword,
+    };
+
+    const validationResult = signupSchema.safeParse(invalidData);
+    expect(validationResult.success).toBe(false);
+  });
+
+  it("비밀번호가 서로 다를때 실패해야 함", () => {
+    const invalidData = {
+      email: testData.validEmail,
+      password: testData.validPassword,
+      confirmPassword: testData.mismatchedPassword,
+    };
+
+    const validationResult = signupSchema.safeParse(invalidData);
+    expect(validationResult.success).toBe(false);
+  });
+});
