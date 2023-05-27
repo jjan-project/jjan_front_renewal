2cbf86caaddf1a67fc11f5472a1cf367e8037740
Choi Hong Gyu <zxolqs@gmail.com>
2023-05-27 12:01:42 +0900

    [#12] test : Extend expect test function

--- src/setup/setupTests.ts
+++ src/setup/setupTests.ts
@@ -1,6 +1,9 @@
-import { afterAll, afterEach, beforeAll } from "vitest";
+import matchers from "@testing-library/jest-dom/matchers";
+import { expect, afterAll, afterEach, beforeAll } from "vitest";
 
 import { server } from "@/mocks/server";
+
+expect.extend(matchers);
 
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterAll(() => server.close());
