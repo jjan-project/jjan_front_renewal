a5b1269dea446e0aedd9c9a4d99bb906903e2bae
MinGyu-Kang <kagrin97@gmail.com>
2023-05-29 11:10:08 +0900

    [#28] feat : Creating and Applying a QueryProvider Component

---
+++ src/queryProvider.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import QueryProvider from "./queryProvider";
 
 import { SignupProvider } from "@/store/signupStore";
 
@@ -48,17 +48,14 @@ const Router = () => {
     );
   }
 
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <QueryProvider>
       <BrowserRouter>
         {/* <MainNavigation /> */}
         <Suspense fallback={<LoadingSpinner />}>{routes}</Suspense>
         {/* <BottomNavigation /> */}
       </BrowserRouter>
-    </QueryClientProvider>
+    </QueryProvider>
   );
 };
 
