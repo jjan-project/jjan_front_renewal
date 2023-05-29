edfae2f2cb9fba1a22f3a261b2e23a36256cda84
MinGyu-Kang <kagrin97@gmail.com>
2023-05-29 11:38:02 +0900

    [#28] test : Add invalid input test code to useSignUp.test
    	useSignUp.test에 잘못된 회원가입 입력 테스트코드 추가 및 중복된 코드 리팩토링했습니다.

--- src/hooks/__test__/useSignUp.test.tsx
+++ src/hooks/__test__/useSignUp.test.tsx
@@ -1,55 +1,57 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { render, act, fireEvent, waitFor } from "@testing-library/react";
 import React from "react";
+import { render, act, fireEvent, waitFor } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemoryRouter } from "react-router-dom";
 import { describe, expect, test } from "vitest";
 
 import { useSignUp } from "../useSignUp";
-
 import { QUERY_KEY } from "@/constants/queryKeys";
 import { testBaseURL } from "@/constants/testBaseURL";
 
+let signUpData = {
+  email: "user@example.com",
+  password: "password123",
+  nickname: "John",
+  location: "Seoul",
+  birthday: "",
+  avatar: "",
+  gender: "",
+  capacity: "",
+};
+
+let queryClient: QueryClient;
+
+const renderWithProviders = (children: React.ReactNode) => {
+  queryClient = new QueryClient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </QueryClientProvider>,
+  );
+};
+
 describe("useSignUp", () => {
-  test("회원가입 성공 시 onSuccess 함수가 호출되고 쿼리 데이터가 업데이트되는지 확인", async () => {
-    const signUpData = {
-      email: "user@example.com",
-      password: "password123",
-      nickname: "John",
-      location: "Seoul",
-      birthday: "",
-      avatar: "",
-      gender: "",
-      capacity: "",
+  const TestComponent = () => {
+    const signUpMutation = useSignUp(`${testBaseURL}/user/join`);
+    const handleClick = () => {
+      signUpMutation.mutate(signUpData);
     };
 
-    const queryClient = new QueryClient();
-    const TestComponent = () => {
-      const signUpMutation = useSignUp(`${testBaseURL}/user/join`);
-      const handleClick = () => {
-        signUpMutation.mutate(signUpData);
-      };
-
-      return (
-        <div>
-          <button onClick={handleClick}>버튼</button>
-        </div>
-      );
-    };
-
-    const { getByText } = render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <TestComponent />
-        </MemoryRouter>
-      </QueryClientProvider>,
+    return (
+      <div>
+        <button onClick={handleClick}>버튼</button>
+      </div>
     );
+  };
 
+  test("회원가입 성공 시 onSuccess 함수가 호출되고 쿼리 데이터가 업데이트되는지 확인", async () => {
+    const { getByText } = renderWithProviders(<TestComponent />);
     const button = getByText("버튼");
+
     fireEvent.click(button);
 
     await act(async () => {
-      // onSuccess 콜백의 비동기 동작이 완료될 때까지 대기
-      // setTimeout을 사용하지 않는 방법을 찾지못함
       await new Promise(resolve => setTimeout(resolve, 100));
     });
 
@@ -59,6 +61,30 @@ describe("useSignUp", () => {
       };
 
       expect(message).toEqual("OK");
+    });
+  });
+
+  test("이메일이 빈 문자열일 때 'FORBIDDEN' 메시지가 반환되는지 확인", async () => {
+    signUpData = {
+      ...signUpData,
+      email: "",
+    };
+
+    const { getByText } = renderWithProviders(<TestComponent />);
+    const button = getByText("버튼");
+
+    fireEvent.click(button);
+
+    await act(async () => {
+      await new Promise(resolve => setTimeout(resolve, 100));
+    });
+
+    await waitFor(() => {
+      const { message } = queryClient.getQueryData([QUERY_KEY.user]) as {
+        message: string;
+      };
+
+      expect(message).toEqual("FORBIDDEN");
     });
   });
 });
