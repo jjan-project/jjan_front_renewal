ec65b760feefb1349bef9b142fc416692b4d5f73
MinGyu-Kang <kagrin97@gmail.com>
2023-06-07 11:58:26 +0900

    [#32] feat : Return additional isLoadingGeoLocation and errorGeoLocation values from useGeoLocation
    	useGeoLocation에서 isLoadingGeoLocation errorGeoLocation값을 추가로 리턴
    	isLoadingGeoLocation은 로딩상태를 Booelan으로 리턴
    	errorGeoLocation 객체는 isError라는 Boolean값과
    	errorMessage 문자열을 리턴

--- src/hooks/useGeoLocation.ts
+++ src/hooks/useGeoLocation.ts
@@ -2,29 +2,57 @@ import { useEffect, useState } from "react";
 
 import type { Coordinates } from "@/types/location.type";
 
-export function useGeoLocation(): Coordinates {
-  const [location, setLocation] = useState<Coordinates>({
+interface Error {
+  isError: boolean;
+  errorMessage: string;
+}
+
+interface GeoLocationReturn {
+  geoLocation: Coordinates;
+  isLoadingGeoLocation: boolean;
+  errorGeoLocation: Error;
+}
+
+export function useGeoLocation(): GeoLocationReturn {
+  const [geoLocation, setGeoLocation] = useState<Coordinates>({
     latitude: 0,
     longitude: 0,
+  });
+
+  const [isLoadingGeoLocation, setIsLoadingGeoLocation] =
+    useState<boolean>(true);
+
+  const [errorGeoLocation, setErrorGeoLocation] = useState<Error>({
+    isError: false,
+    errorMessage: "",
   });
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         position => {
-          setLocation({
+          setGeoLocation({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
           });
+          setIsLoadingGeoLocation(false);
         },
         error => {
-          console.error(error.message);
+          setErrorGeoLocation({
+            isError: true,
+            errorMessage: error.message,
+          });
+          setIsLoadingGeoLocation(false);
         },
       );
     } else {
-      console.error("Geolocation이 해당 브라우저에서 지원되지 않습니다.");
+      setErrorGeoLocation({
+        isError: true,
+        errorMessage: "Geolocation이 해당 브라우저에서 지원되지 않습니다.",
+      });
+      setIsLoadingGeoLocation(false);
     }
   }, []);
 
-  return location;
+  return { geoLocation, isLoadingGeoLocation, errorGeoLocation };
 }
