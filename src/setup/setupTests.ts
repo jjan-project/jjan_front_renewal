e46636ee923af289ccbd93ce7ee6e4f1ca5ad04f
Choi Hong Gyu <zxolqs@gmail.com>
2023-06-09 14:58:25 +0900

    [#37] Fix : Error fix

--- src/setup/setupTests.ts
+++ src/setup/setupTests.ts
@@ -3,8 +3,6 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 
 import { server } from "@/mocks/server";
 
-expect.extend(matchers);
-
 beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
