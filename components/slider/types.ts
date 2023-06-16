dfa80f62fee8dbfab408421d6e9a544bec870797
Choi Hong Gyu <zxolqs@gmail.com>
2023-06-16 18:49:25 +0900

    [#45] feat : Add types

---
+++ components/slider/types.ts
@@ -0,0 +1,34 @@
+import type { InputHTMLAttributes, Dispatch, SetStateAction } from "react";
+
+import { BaseTest } from "../types/base";
+
+type SliderOmitProps = "type" | "min" | "max" | "step" | "value";
+
+type SliderProps = Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  SliderOmitProps
+> &
+  BaseTest & {
+    /**
+     * The minimum value of the slider
+     */
+    min: number;
+    /**
+     * The maximum value of the slider
+     */
+    max: number;
+    /**
+     * The increment between valid values of the slider
+     */
+    step?: number;
+    /**
+     * The current value of the slider
+     */
+    value: number;
+    /**
+     * Function to update the value of the slider
+     */
+    setValue: Dispatch<SetStateAction<number>>;
+  };
+
+export { SliderProps };
