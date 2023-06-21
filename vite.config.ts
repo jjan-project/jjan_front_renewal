4643469a3458fd122f134247ed18ac0a753cc92c
Choi Hong Gyu <zxolqs@gmail.com>
2023-06-21 23:24:06 +0900

    [#50] feat : Setup vanilla extract config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 /// <reference types="vitest" />
-
 import { fileURLToPath, URL } from "url";
 
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
