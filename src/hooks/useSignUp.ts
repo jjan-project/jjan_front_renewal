cd4b3ff53e383ea67030583057680941d81b644b
MinGyu-Kang <kagrin97@gmail.com>
2023-06-25 16:04:39 +0900

    [#53] feat : Refresh token to cookie in the useSignUp hook and store the access token in the state

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -2,8 +2,20 @@ import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 
 import { QUERY_KEY } from "@/constants/queryKeys";
+import { cookieManager } from "@/module/cookie";
+import { KEY } from "@/module/cookie/constant";
 import { httpService } from "@/module/http";
 import { State } from "@/store/signupStore";
+
+type DataType = {
+  item: string;
+  target: string;
+  message: string;
+  token: {
+    accessToken: string;
+    refreshToken?: string;
+  };
+};
 
 export function useSignUp(baseURL: string) {
   const navigate = useNavigate();
@@ -11,9 +23,15 @@ export function useSignUp(baseURL: string) {
   const queryClient = useQueryClient();
 
   const signUpMutation = useMutation(
-    (userData: State) => httpService.post(baseURL, userData),
+    (userData: State): Promise<DataType> => httpService.post(baseURL, userData),
     {
-      onSuccess: data => {
+      onSuccess: (data: DataType) => {
+        if (data.message === "OK") {
+          const { refreshToken, ...restToken } = data.token;
+          data = { ...data, token: restToken };
+          cookieManager.set(KEY.JWT_REFRESH_TOKEN, refreshToken);
+        }
+
         queryClient.setQueryData([QUERY_KEY.user], data);
         navigate("/auth/signup/complete"); // 추후 수정 예정
       },
