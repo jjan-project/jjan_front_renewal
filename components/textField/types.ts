499714cee34595059c7acadc26fd47e70e6d3c6a
MinGyu-Kang <kagrin97@gmail.com>
2023-06-30 13:05:23 +0900

    [#60] feat : ADD Text Area component

---
+++ components/textField/types.ts
@@ -0,0 +1,13 @@
+import { TextareaHTMLAttributes } from "react";
+
+import { BaseTest } from "../types/base";
+
+type TextAreaBaseProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+type TextAreaProps = TextAreaBaseProps &
+  BaseTest & {
+    width?: string;
+    height?: string;
+  };
+
+export { TextAreaProps };
