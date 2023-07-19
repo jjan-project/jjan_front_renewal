3d27f7d717dac67425b1a0dd0b6a871ddbb49b1a
Choi Hong Gyu <zxolqs@gmail.com>
2023-07-20 03:08:30 +0900

    [#92] feat : Add reset css rules

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import Router from "./router";
 
+import "./style/global.css";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router />
