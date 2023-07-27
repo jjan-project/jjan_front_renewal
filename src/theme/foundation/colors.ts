2b531a84164d5be27c168a471e399b9379183db4
Choi Hong Gyu <zxolqs@gmail.com>
2023-07-27 23:42:07 +0900

    [#107] feat : Add color and space foundations

--- src/theme/foundation/colors.ts
+++ src/theme/foundation/colors.ts
@@ -5,6 +5,8 @@ export const colors = {
   gray200: "#3E3E45",
   gray300: "#4D4D57",
   gray600: "#A6A6B9",
+  gray700: "#C4C4C4",
+  green175: "#AFF73C",
   orange300: "#F2822A",
   violet100: "#5B1FD9",
   violet200: "#6230CB",
