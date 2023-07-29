5e9a475477cc4b04d130ef02d896c5cc265320ea
Choi Hong Gyu <zxolqs@gmail.com>
2023-07-29 10:33:30 +0900

    [#67] feat : Add routes for join apis

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,11 @@
 export const kakaoRoutes = {
   searchKeyword: "https://dapi.kakao.com/v2/local/search/keyword.json",
 };
+
+export const join = {
+  checkUniqueEmail: "/api/user/unique-email",
+  checkUniqueNickname: "/api/user/unique-nickname",
+  getRandomNickname: "/api/user/random-nickname",
+  signin: "api/user/login",
+  signup: "api/user/join",
+};
