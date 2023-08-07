acaf3971e10f2f6535c0c83f36db30ba2879c3a1
MinGyu-Kang <kagrin97@gmail.com>
2023-08-07 16:54:37 +0900

    [#115] feat : notifications UI

---
+++ src/pages/notifications/Notifications.tsx
@@ -0,0 +1,71 @@
+import { IconChevronLeftLarge, IconPeopleGroup } from "jjan-icon";
+
+import { Box } from "@/components/box";
+import { Flex } from "@/components/flex";
+import { Header } from "@/components/header";
+import { List } from "@/components/list";
+import { Spacing } from "@/components/spacing";
+import { Stack } from "@/components/stack";
+import { Typo } from "@/components/typo";
+
+const CardInfo = () => {
+  return (
+    <Stack>
+      <Box>
+        <Typo
+          appearance="body2"
+          color="violet100"
+          style={{ fontWeight: "bold" }}
+        >
+          회기 꽃술 6인팟!!
+        </Typo>{" "}
+        <Typo appearance="body2" color="black" style={{ fontWeight: "bold" }}>
+          모임이 개설되었어요.
+        </Typo>
+      </Box>
+
+      <Typo appearance="body3" color="black" style={{ fontWeight: "bold" }}>
+        30초 전
+      </Typo>
+    </Stack>
+  );
+};
+
+const CardIcon = () => {
+  return (
+    <Box
+      padding="4px"
+      width="45px"
+      height="45px"
+      backgroundColor="gray700"
+      borderRadius="15px"
+    >
+      <IconPeopleGroup width="100%" height="100%" />
+    </Box>
+  );
+};
+
+const Card = () => {
+  return (
+    <Flex gap="18px" alignItems="center">
+      <CardIcon />
+      <CardInfo />
+    </Flex>
+  );
+};
+
+const Notifications = () => {
+  return (
+    <Box height="100dvh" padding="0 20px">
+      <Header leftIcon={<IconChevronLeftLarge />}>알림</Header>
+      <Spacing direction="vertical" size="20px" />
+      <List gap="30px" height="calc(100dvh - 20px)">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Card key={index} />
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export { Notifications };
