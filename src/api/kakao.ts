3784f0b20943a9f9916bd735d08c8640dd1e3bee
Choi Hong Gyu <zxolqs@gmail.com>
2023-08-17 13:21:24 +0900

    Add useGetLocation example api

--- src/api/kakao.ts
+++ src/api/kakao.ts
@@ -13,3 +13,19 @@ export const useGetNeighborhoods = (params: PlacesSearchOptionsStringified) =>
     config: { enabled: !(params.x === "0" && params.y === "0") },
     customQueryKey: QUERY_KEY.kakaoNeighborhoods,
   });
+
+type ExParam = {
+  x: string;
+  y: string;
+  radius: string;
+  query: string;
+};
+
+export const useGetLocation = (params: ExParam) =>
+  serverStateManager.fetch<PlacesSearchResultResponse>({
+    url: `${kakaoRoutes.searchKeyword}?x=${params.x}&y=${params.y}&radius=${params.radius}&query=${params.query}`,
+    config: {
+      enabled: !!params.query,
+    },
+    customQueryKey: `${QUERY_KEY.locations}${params.query}`,
+  });
