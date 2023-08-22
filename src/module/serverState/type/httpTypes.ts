200b3d068ea8083464139abbd851fcf96206cc91
MinGyu-Kang <kagrin97@gmail.com>
2023-08-22 12:58:02 +0900

    [#123] feat : AxiosHttpHandler's setupErrorInterceptor code
    	AxiosHttpHandler's setupErrorInterceptor code에 ErrorCompoenets의
    	error객체를 커스텀화 하도록 변경

--- src/module/serverState/type/httpTypes.ts
+++ src/module/serverState/type/httpTypes.ts
@@ -1,7 +1,13 @@
 import { AxiosError, AxiosResponse, AxiosRequestConfig } from "axios";
 
+type ResponseTypeJjan<T> = {
+  code: number;
+  message: string;
+  data: T;
+};
+
 type RequestConfigType = AxiosRequestConfig;
-type ResponseType<T = unknown> = AxiosResponse<T>;
+type ResponseType<T = unknown> = AxiosResponse<T> & ResponseTypeJjan<T>;
 type ErrorType = AxiosError;
 
 export type { RequestConfigType, ResponseType, ErrorType };
