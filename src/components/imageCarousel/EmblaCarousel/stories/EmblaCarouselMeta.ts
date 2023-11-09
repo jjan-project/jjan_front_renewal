579a59da42e7b974c58151fe6597c746a9844754
MinGyu-Kang <kagrin97@gmail.com>
2023-11-09 23:06:03 +0900

    fix : vercel not found modules

--- src/components/imageCarousel/EmblaCarousel/stories/EmblaCarouselMeta.ts
+++ src/components/imageCarousel/EmblaCarousel/stories/EmblaCarouselMeta.ts
@@ -1,6 +1,22 @@
 import { IconChevronLeftSmall, IconChevronRightSmall } from "jjan-icon";
 
-import { DISABLE, Controls } from "../../../../../.storybook/constants";
+const DISABLE = { table: { disable: true } };
+
+const Controls = {
+  BOOLEAN: { type: "boolean" },
+  RADIO: { type: "radio" },
+  NUMBER: { type: "number" },
+  INLINE_RADIO: { type: "inline-radio" },
+  SELECT: { type: "select" },
+  CHEKC: { type: "check" },
+  MULTI_SELECT: { type: "multi-select" },
+  JSON: { type: "object" },
+  RANGE: { type: "range" },
+  FILE: { type: "file" },
+  COLOR: { type: "color" },
+  DATE: { type: "date" },
+  text: { type: "text" },
+};
 
 const Categories = {
   OPTIONS: "options",
