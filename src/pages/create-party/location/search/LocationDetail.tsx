6f49fba8291f6e26ab5acd917e3e6abf6783eec1
gomgun <zxolqs@gmail.com>
2024-02-08 07:38:03 +0900

    [#184] feat : add useDebounce hook (#186)

--- src/pages/create-party/location/search/LocationDetail.tsx
+++ src/pages/create-party/location/search/LocationDetail.tsx
@@ -7,6 +7,7 @@ import { Box } from "@/components/box";
 import { Flex } from "@/components/flex";
 import { Header } from "@/components/header";
 import { Input } from "@/components/input";
+import useDebounce from "@/hooks/useDebounce";
 import { PartyLocation } from "@/store/partyStore";
 
 type LocationSearchProps = {
@@ -18,6 +19,7 @@ type LocationSearchProps = {
 
 const LocationSearch = (props: LocationSearchProps) => {
   const [searchValue, setSearchValue] = useState<string>("");
+  const debouncedValued = useDebounce<string>(searchValue, 300);
 
   const handleClose = () => {
     props.onClose(prev => !prev);
@@ -49,7 +51,7 @@ const LocationSearch = (props: LocationSearchProps) => {
           isValid
         />
         <Suspense fallback={<h1>fallback</h1>}>
-          <LocationList {...props} searchValue={searchValue} />
+          <LocationList {...props} searchValue={debouncedValued} />
         </Suspense>
       </Flex>
     </Box>
